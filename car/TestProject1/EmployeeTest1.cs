a7b4077d7995c636dac7c227b3988a31a8cc5ede
Mel Keilwert <keilwertmelg@gmail.com>
2025-02-11 16:09:08 -0700

    working on lists

--- car/TestProject1/EmployeeTest1.cs
+++ car/TestProject1/EmployeeTest1.cs
@@ -183,6 +183,42 @@ namespace EmployeeTests
                             }
                         } while(!buttonClick);
                     }
+                    static void Main11()
+                    {
+                        List<string> citiesList = new List<string> { "Delhi", "Los Angeles", "Saint Petersburg" };
+      
+                        citiesList.Add("New York City");
+      
+                        citiesList.Remove("Delhi");
+      
+                        citiesList.AddRange(new string[] {"Cairo", "Johannesburg"});
+      
+                        bool hasNewDelhi = citiesList.Contains("New Delhi");
+      
+                        foreach (string city in citiesList)
+                        {
+                            Console.WriteLine(city);
+                            
+                            // an example of a list
+                        }
+                    }
+                    static void Main12()
+                    {
+                        List<double> marathons = new List<double>();
+                        marathons.Add(144.07);
+                        marathons.Add(143.12);
+                        Console.WriteLine(marathons[1]);
+                    }
+                    
+                    static void Main13()
+                    {
+                        List<double> marathons = new List<double> {144.07, 143.12, 146.73, 146.33};
+      
+                        double time = marathons[1];
+      
+                        Console.WriteLine($"The 2012 marathon was ran in {time} minutes!");
+
+                    }
     }
 }
     
